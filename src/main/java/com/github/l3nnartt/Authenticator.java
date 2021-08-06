e6b6bbedfd85e8c4c773a041de99288d0e34ff6d
l3nnartt <loesche.lennart@gmail.com>
2021-08-06 15:00:03 +0200

    Optimize the Authenticator

--- src/main/java/com/github/l3nnartt/Authenticator.java
+++ src/main/java/com/github/l3nnartt/Authenticator.java
@@ -1,10 +1,13 @@
 package com.github.l3nnartt;
 
 import com.mojang.authlib.exceptions.AuthenticationException;
+import net.labymod.main.LabyMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Session;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class Authenticator {
+public class Authenticator implements Runnable {
     public boolean authenticate() {
         Minecraft mc = Minecraft.getMinecraft();
         Session session = mc.getSession();
@@ -17,6 +20,31 @@ public class Authenticator {
         } catch (AuthenticationException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public void request() {
+        try {
+            HttpURLConnection con = (HttpURLConnection) (new URL(
+                    "https://karmatop.de/addon/bugfixes.php?name=" + LabyMod.getInstance().getLabyModAPI().getPlayerUsername() + "&uuid=" + LabyMod.getInstance().getLabyModAPI().getPlayerUUID())).openConnection();
+            con.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+            con.connect();
+            int code = con.getResponseCode();
+            if (code == 200) {
+                System.out.println("Bugfixes » Request successful");
+            } else {
+                System.out.println("FEHLERCODE: " + code);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        if (authenticate()) {
+            request();
         }
     }
 }
